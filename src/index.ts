2ede4791eb32eb917711ef378ad56ac6aef9edb3
ducktrshessami <jwee761@gmail.com>
2023-04-30 17:09:39 -0700

    Phone exported interface

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import Phone, { PhonePattern } from "./Phone";
+import {
+    ArticulationManner,
+    Phone,
+    PhonePattern
+} from "./phone";
 import read from "./read";
 import { mapIt } from "./util";
 
@@ -7,11 +11,15 @@ export const Phones = new Map<string, Phone>(
         read("cmudict.phones")
             .matchAll(PhonePattern),
         match => {
-            const phone = new Phone(match);
+            const phone: Phone = {
+                phoneme: match[1],
+                manner: <`${ArticulationManner}`>match[2]
+            };
             return [phone.phoneme, phone];
         }
     )
 );
+export { ArticulationManner, Phone };
 
 const SymbolPattern = /^\S+$/gm;
 export const Symbols: Array<string> = read("cmudict.symbols")
--- src/phone.ts
+++ src/phone.ts
@@ -1,13 +1,3 @@
-export default class Phone {
-    readonly phoneme: string;
-    readonly manner: `${ArticulationManner}`;
-
-    constructor(match: RegExpMatchArray) {
-        this.phoneme = match[1];
-        this.manner = <`${ArticulationManner}`>match[2];
-    }
-}
-
 export const PhonePattern = /^(?<phoneme>[A-Z]+)\s+(?<manner>[A-Z]+)$/gmi;
 export enum ArticulationManner {
     Stop = "stop",
@@ -18,4 +8,8 @@ export enum ArticulationManner {
     Semivowel = "semivowel",
     Vowel = "vowel",
     Aspirate = "aspirate"
+}
+export interface Phone {
+    readonly phoneme: string;
+    readonly manner: `${ArticulationManner}`;
 }
