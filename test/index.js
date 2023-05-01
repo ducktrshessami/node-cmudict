d4531c8c6d11f18cedd5bd93456eeab8e4fd46ad
ducktrshessami <jwee761@gmail.com>
2023-05-01 00:58:11 -0700

    Dict test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,17 @@
 const assert = require("assert");
 const cmudict = require("../dist");
+const { readFileSync } = require("fs");
+
+it("Dict covers all lines", function () {
+    const lines = readFileSync(require.resolve("cmudict/cmudict.dict"), "utf8")
+        .split("\n")
+        .filter(line => line);
+    let pronunciations = 0;
+    for (const entry of cmudict.Dict.values()) {
+        pronunciations += entry.pronunciations.length;
+    }
+    assert.strictEqual(pronunciations, lines.length);
+});
 
 it("all phones' manners in ArticulationManner enum", function () {
     const manners = Object.values(cmudict.ArticulationManner);
