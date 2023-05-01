780b09024e20533385a4171d3d2be6fbb0e833d3
ducktrshessami <jwee761@gmail.com>
2023-04-30 20:55:39 -0700

    vp parsing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Entry } from "./entry";
 import { ArticulationManner, PhonePattern } from "./phone";
 import read from "./read";
 import { mapIt } from "./util";
@@ -17,3 +18,12 @@ export { ArticulationManner };
 const SymbolPattern = /^\S+$/gm;
 export const Symbols: Array<string> = read("cmudict.symbols")
     .match(SymbolPattern)!;
+
+// WTF does 'vp' mean?
+const VPPattern = /^(?<name>[^A-Z\s]+)(?<note>[^\s()]+)?(?:\((?<index>\d+)\))? (?<phonemes>(?:[A-Z]+[0-2]?(?: |$))+)/mgi;
+export const VP = new Map<string, Entry>();
+for (const match of read("cmudict.vp").matchAll(VPPattern)) {
+    const entry = VP.get(match.groups!.name) ?? new Entry(match.groups!.name);
+    entry.addPronunciation(match.groups!.phonemes.split(" "), match.groups!.note);
+    VP.set(entry.name, entry);
+}
