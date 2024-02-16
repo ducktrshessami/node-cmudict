467af152095c73e02945a14d1d66807ba0377d1d
ducktrshessami <jwee761@gmail.com>
2024-02-15 20:15:54 -0800

    refactor: cmudict reading methods

--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,32 @@ import {
     readPronunciations
 } from "./util";
 
-const WordPattern = /^(?<name>[^\s()]+)(?:\((?<index>\d+)\))? (?<phonemes>(?:[A-Z]+[0-2]?(?:(?= #)| |$))+)(?: # (?<tags>(?:[^\s,]+(?:$|[\s,]+))+))?$/gmi;
-export const Dict = readPronunciations("cmudict.dict", WordPattern);
+export function getDict(): Map<string, Entry> {
+    return readPronunciations("cmudict.dict", /^(?<name>[^\s()]+)(?:\((?<index>\d+)\))? (?<phonemes>(?:[A-Z]+[0-2]?(?:(?= #)| |$))+)(?: # (?<tags>(?:[^\s,]+(?:$|[\s,]+))+))?$/gmi);
+}
 
-const PhonePattern = /^(?<phoneme>[A-Z]+)\s+(?<manner>[A-Z]+)$/gmi;
 /**
  * Maps phoneme to manner of articulation
  */
-export const Phones = new Map<string, `${ArticulationManner}`>(
-    mapIt(
-        read("cmudict.phones")
-            .matchAll(PhonePattern),
-        match => [match[1], <`${ArticulationManner}`>match[2]]
-    )
-);
+export function getPhones(): Map<string, `${ArticulationManner}`> {
+    return new Map<string, `${ArticulationManner}`>(
+        mapIt(
+            read("cmudict.phones")
+                .matchAll(/^(?<phoneme>[A-Z]+)\s+(?<manner>[A-Z]+)$/gmi),
+            match => [match[1], <`${ArticulationManner}`>match[2]]
+        )
+    );
+}
 
-const SymbolPattern = /^\S+$/gm;
-export const Symbols: Array<string> = read("cmudict.symbols")
-    .match(SymbolPattern)!;
+export function getSymbols(): string[] {
+    return read("cmudict.symbols")
+        .match(/^\S+$/gm)!;
+}
 
 // WTF does 'vp' mean?
-const VPPattern = /^(?<name>[^A-Z\s]+)(?<note>[^\s()]+)?(?:\((?<index>\d+)\))? (?<phonemes>(?:[A-Z]+[0-2]?(?: |$))+)/gmi;
-export const VP = readPronunciations("cmudict.vp", VPPattern);
+export function getVP(): Map<string, Entry> {
+    return readPronunciations("cmudict.vp", /^(?<name>[^A-Z\s]+)(?<note>[^\s()]+)?(?:\((?<index>\d+)\))? (?<phonemes>(?:[A-Z]+[0-2]?(?: |$))+)/gmi);
+}
 
 export { ArticulationManner, Entry };
 export { Phoneme, Pronunciation } from "./entry";
--- test/index.mjs
+++ test/index.mjs
@@ -18,26 +18,34 @@ function pronunciationCount(map) {
 }
 
 describe("ESM", function () {
+    let Dict, Phones, Symbols, VP;
+    before("reading all cmudict features", function () {
+        Dict = cmudict.getDict();
+        Phones = cmudict.getPhones();
+        Symbols = cmudict.getSymbols();
+        VP = cmudict.getVP();
+    });
+
     it("Dict covers all lines", function () {
-        assert.strictEqual(pronunciationCount(cmudict.Dict), lineCount("cmudict.dict"));
+        assert.strictEqual(pronunciationCount(Dict), lineCount("cmudict.dict"));
     });
 
     it("all phones' manners in ArticulationManner enum", function () {
         const manners = Object.values(cmudict.ArticulationManner);
-        for (const manner of cmudict.Phones.values()) {
+        for (const manner of Phones.values()) {
             assert(manners.includes(manner));
         }
     });
 
     it("Phones covers all lines", function () {
-        assert.strictEqual(cmudict.Phones.size, lineCount("cmudict.phones"));
+        assert.strictEqual(Phones.size, lineCount("cmudict.phones"));
     });
 
     it("Symbols covers all lines", function () {
-        assert.strictEqual(cmudict.Symbols.length, lineCount("cmudict.symbols"));
+        assert.strictEqual(Symbols.length, lineCount("cmudict.symbols"));
     });
 
     it("VP covers all lines", function () {
-        assert.strictEqual(pronunciationCount(cmudict.VP), lineCount("cmudict.vp"));
+        assert.strictEqual(pronunciationCount(VP), lineCount("cmudict.vp"));
     });
 });
