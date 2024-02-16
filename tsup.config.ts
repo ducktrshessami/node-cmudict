75436ef336c57255efcc09831bc9c378ecf598ea
ducktrshessami <jwee761@gmail.com>
2024-02-15 17:51:45 -0800

    chore: deps + split build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-    entry: ["src/index.ts"],
-    clean: true,
-    dts: true,
-    sourcemap: true,
-    format: ["cjs"],
-    minifySyntax: true,
-    minifyWhitespace: true
+export default defineConfig(options => {
+    if (!Array.isArray(options.entry)) {
+        throw new Error("Invalid entry");
+    }
+    const install = options.entry.includes("src/install.ts");
+    return {
+        dts: !install,
+        sourcemap: true,
+        format: install ? "esm" : ["esm", "cjs"],
+        minifySyntax: true,
+        minifyWhitespace: true
+    };
 });
